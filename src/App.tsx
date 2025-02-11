1041ec32b32e4e41a91f7a2581f2460b77fa01a8
Andrew Vasquez <andrewvasquez0113@gmail.com>
2025-02-10 19:58:50 -0800

    pocketbase connection working

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery } from "@tanstack/react-query";
 
-function App() {
-  const [count, setCount] = useState(0)
+//const fetchReservations = async () => {
+//  const records = await pb.collection("reservations").getList(1, 50, {
+//    sort: "-created",
+//  });
+//  return records.items;
+//};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+  //const {
+  //  isLoading,
+  //  isError,
+  //  data: reservations,
+  //  error,
+  //} = useQuery({
+  //  queryKey: ["reservations"], // Unique key for the query
+  //  queryFn: fetchReservations,
+  //});
+
+  //if (isLoading) {
+  //  return <p>Loading posts...</p>;
+  //}
+  //
+  //if (isError) {
+  //  return <p>Error: {error.message}</p>;
+  //}
+
+  return <main></main>;
 }
-
-export default App
