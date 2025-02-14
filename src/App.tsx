d4579b17c6255b219f767720ba54043e38c0cf81
Brion Lund <me@brionispopt.art>
2025-02-13 23:14:04 -0800

    added font files

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
 import React from "react"
 import "./App.css"
 
-// shadcn UI components (adjust import paths as needed)
 import { Separator } from "@/components/ui/separator"
-
-// Local components
 import LeftSection from "./components/left-section"
-import RightSection from "./components/right-section"
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Separator />
-      <div className="flex flex-col lg:flex-row">
+      <div className="">
         <LeftSection />
-        <RightSection />
       </div>
-    </div>
+    </>
   )
 }
 
