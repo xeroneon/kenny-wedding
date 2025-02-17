536dd8c7cfdbfa973da277ee4b0c92827be2b34f
Brion Lund <me@brionispopt.art>
2025-02-16 22:46:48 -0800

    payment info added

--- src/components/honeymoon-fund.tsx
+++ src/components/honeymoon-fund.tsx
@@ -1,26 +1,31 @@
-
 export default function HoneymoonFund() {
-  return (
-    <section id="honeymoon-fund" className="space-y-4">
-      <h2 className="text-3xl font-bold underline underline-offset-4">
-        Honeymoon Fund
-      </h2>
-      <p>
-        In lieu of a registry for wedding gifts, we have set up a honeymoon fund
-        and contributions towards the honeymoon are welcome! Your presence is
-        the greatest gift, but if you wish to give something, it would be much
-        appreciated!
-      </p>
-      <p>
-        We are taking a cruise to Alaska! While Kenny may have lived there, it's
-        been a bucketlist item of Jessica's. We are beyond excited to make our
-        own memories!
-      </p>
-      <p>
-        <strong>Zelle:</strong> 209-628-5858 or 925-858-5477
-        <br />
-        <strong>Venmo:</strong> {/* Insert Venmo username/link here */}
-      </p>
-    </section>
-  );
+    return (
+        <section id="honeymoon-fund" className="space-y-4 flex gap-4">
+            <div className="flex-1">
+                <h2 className="text-3xl font-bold underline underline-offset-4">
+                    Honeymoon Fund
+                </h2>
+                <p>
+                    In lieu of a registry for wedding gifts, we have set up a honeymoon fund
+                    and contributions towards the honeymoon are welcome! Your presence is
+                    the greatest gift, but if you wish to give something, it would be much
+                    appreciated!
+                </p>
+                <p>
+                    We are taking a cruise to Alaska! While Kenny may have lived there, it's
+                    been a bucketlist item of Jessica's. We are beyond excited to make our
+                    own memories!
+                </p>
+            </div>
+            <div className="flex-1">
+                <p>
+                    <strong>Zelle:</strong> 209-628-5858 or 925-858-5477
+                </p>
+                <p>
+                    
+                    <img src="src/assets/venmo.png" alt="Venmo" className="max-h-48 inline" />
+                </p>
+            </div>
+        </section>
+    );
 }
