f790895e0b273983c07c8f5a03d13ca84846832e
Andrew Vasquez <andrewvasquez0113@gmail.com>
2025-02-26 14:54:17 -0800

    fixed rsvp button not showing on smaller phones

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import "./App.css"
+import "./App.css";
 
-import { Separator } from "@/components/ui/separator"
-import LeftSection from "./components/left-section"
-import RightSection from "./components/right-section"
+import LeftSection from "./components/left-section";
+import RightSection from "./components/right-section";
 import { Toaster } from "./components/ui/toaster";
 
 function App() {
-
   return (
     <>
-      <Separator />
       <div className="flex flex-col lg:flex-row">
         <div className="w-full lg:w-1/2 xl:w-1/3 lg:fixed">
           <LeftSection />
